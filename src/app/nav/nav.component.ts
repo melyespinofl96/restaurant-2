1ca77bc2f7649bc9fb96970e6d71f54e616df9de
Mely Espino <90160548+melyespinofl96@users.noreply.github.com>
2023-06-30 23:30:24 -0500

    update

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 
 export class NavComponent implements OnInit{
 
-  mobileMenu = false
+  mobileMenu:boolean = false
 
   ngOnInit(): void {
     this.mobileMenu = false
