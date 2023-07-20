c23c7c8b4c7510be9123add1a09d77fef960f7b3
Mely Espino <90160548+melyespinofl96@users.noreply.github.com>
2023-07-19 21:41:47 -0500

    update home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -17,6 +17,9 @@ export class HomeComponent {
   specialItems0: string | undefined;
   specialItems1: string | undefined;
   specialItems2: string | undefined;
+  specialItemsInfo0: string | undefined;
+  specialItemsInfo1: string | undefined;
+  specialItemsInfo2: string | undefined;
 
   constructor(private languageService: LanguageService) {
     this.currentLanguage = this.languageService.getCurrentLanguage();
@@ -29,6 +32,9 @@ export class HomeComponent {
     this.specialItems0 = this.getTranslatedText('specialItems0');
     this.specialItems1 = this.getTranslatedText('specialItems1');
     this.specialItems2 = this.getTranslatedText('specialItems2');
+    this.specialItemsInfo0 = this.getTranslatedText('specialItemsInfo0');
+    this.specialItemsInfo1 = this.getTranslatedText('specialItemsInfo1');
+    this.specialItemsInfo2 = this.getTranslatedText('specialItemsInfo2');
 
     this.languageService.languageChanged.subscribe(language => {
       this.specialsTitle = this.getTranslatedText('specialsTitle');
@@ -37,6 +43,9 @@ export class HomeComponent {
       this.specialItems0 = this.getTranslatedText('specialItems0');
       this.specialItems1 = this.getTranslatedText('specialItems1');
       this.specialItems2 = this.getTranslatedText('specialItems2');
+      this.specialItemsInfo0 = this.getTranslatedText('specialItemsInfo0');
+      this.specialItemsInfo1 = this.getTranslatedText('specialItemsInfo1');
+      this.specialItemsInfo2 = this.getTranslatedText('specialItemsInfo2');
     });
   }
 
@@ -60,6 +69,15 @@ export class HomeComponent {
       },specialItems2: {
         en: translation.Language.en.homePage[0].specialItems[2],
         es: translation.Language.es.homePage[0].specialItems[2]
+      },specialItemsInfo0: {
+        en: translation.Language.en.homePage[0].specialItemsInfo[0],
+        es: translation.Language.es.homePage[0].specialItemsInfo[0]
+      },specialItemsInfo1: {
+        en: translation.Language.en.homePage[0].specialItemsInfo[1],
+        es: translation.Language.es.homePage[0].specialItemsInfo[1]
+      },specialItemsInfo2: {
+        en: translation.Language.en.homePage[0].specialItemsInfo[2],
+        es: translation.Language.es.homePage[0].specialItemsInfo[2]
       },
 
     };
