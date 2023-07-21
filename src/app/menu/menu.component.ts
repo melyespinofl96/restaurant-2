a5a6da30cafff28933d0edf57515db788205c33b
Mely Espino <90160548+melyespinofl96@users.noreply.github.com>
2023-07-20 20:30:27 -0500

    finished translations

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -49,6 +49,37 @@ export class MenuComponent{
   soupsAndSaladsName2: string | undefined;
   soupsAndSaladsInfo2: string | undefined;
 
+  tacosName0: string | undefined;
+  tacosInfo0: string | undefined;
+  tacosName1: string | undefined;
+  tacosInfo1: string | undefined;
+  tacosName2: string | undefined;
+  tacosInfo2: string | undefined;
+  tacosName3: string | undefined;
+  tacosInfo3: string | undefined;
+
+  enchiladasName0: string | undefined;
+  enchiladasInfo0: string | undefined;
+  enchiladasName1: string | undefined;
+  enchiladasInfo1: string | undefined;
+  enchiladasName2: string | undefined;
+  enchiladasInfo2: string | undefined;
+
+  dessertsName0: string | undefined;
+  dessertsInfo0: string | undefined;
+  dessertsName1: string | undefined;
+  dessertsInfo1: string | undefined;
+  dessertsName2: string | undefined;
+  dessertsInfo2: string | undefined;
+
+  beveragesName0: string | undefined;
+  beveragesInfo0: string | undefined;
+  beveragesName1: string | undefined;
+  beveragesInfo1: string | undefined;
+  beveragesName2: string | undefined;
+  beveragesInfo2: string | undefined;
+
+
   constructor(private languageService: LanguageService) {
     this.currentLanguage = this.languageService.getCurrentLanguage();
   }
@@ -79,6 +110,36 @@ export class MenuComponent{
     this.soupsAndSaladsName2 = this.getTranslatedText('soupsAndSaladsName2');
     this.soupsAndSaladsInfo2 = this.getTranslatedText('soupsAndSaladsInfo2');
 
+    this.tacosName0 = this.getTranslatedText('tacosName0');
+    this.tacosInfo0 = this.getTranslatedText('tacosInfo0');
+    this.tacosName1 = this.getTranslatedText('tacosName1');
+    this.tacosInfo1 = this.getTranslatedText('tacosInfo1');
+    this.tacosName2 = this.getTranslatedText('tacosName2');
+    this.tacosInfo2 = this.getTranslatedText('tacosInfo2');
+    this.tacosName3 = this.getTranslatedText('tacosName3');
+    this.tacosInfo3 = this.getTranslatedText('tacosInfo3');
+
+    this.enchiladasName0 = this.getTranslatedText('enchiladasName0');
+    this.enchiladasInfo0 = this.getTranslatedText('enchiladasInfo0');
+    this.enchiladasName1 = this.getTranslatedText('enchiladasName0');
+    this.enchiladasInfo1 = this.getTranslatedText('enchiladasInfo0');
+    this.enchiladasName2 = this.getTranslatedText('enchiladasName0');
+    this.enchiladasInfo2 = this.getTranslatedText('enchiladasInfo0');
+
+    this.dessertsName0 = this.getTranslatedText('dessertsName0');
+    this.dessertsInfo0 = this.getTranslatedText('dessertsInfo0');
+    this.dessertsName1 = this.getTranslatedText('dessertsName0');
+    this.dessertsInfo1 = this.getTranslatedText('dessertsInfo0');
+    this.dessertsName2 = this.getTranslatedText('dessertsName0');
+    this.dessertsInfo2 = this.getTranslatedText('dessertsInfo0');
+
+    this.beveragesName0 = this.getTranslatedText('beveragesName0');
+    this.beveragesInfo0 = this.getTranslatedText('beveragesInfo0');
+    this.beveragesName1 = this.getTranslatedText('beveragesName1');
+    this.beveragesInfo1 = this.getTranslatedText('beveragesInfo1');
+    this.beveragesName2 = this.getTranslatedText('beveragesName2');
+    this.beveragesInfo2 = this.getTranslatedText('beveragesInfo2');
+
     this.languageService.languageChanged.subscribe(language => {
       this.categories0 = this.getTranslatedText('categories0');
       this.categories1 = this.getTranslatedText('categories1');
@@ -103,6 +164,35 @@ export class MenuComponent{
       this.soupsAndSaladsName2 = this.getTranslatedText('soupsAndSaladsName2');
       this.soupsAndSaladsInfo2 = this.getTranslatedText('soupsAndSaladsInfo2');
 
+      this.tacosName0 = this.getTranslatedText('tacosName0');
+      this.tacosInfo0 = this.getTranslatedText('tacosInfo0');
+      this.tacosName1 = this.getTranslatedText('tacosName1');
+      this.tacosInfo1 = this.getTranslatedText('tacosInfo1');
+      this.tacosName2 = this.getTranslatedText('tacosName2');
+      this.tacosInfo2 = this.getTranslatedText('tacosInfo2');
+      this.tacosName3 = this.getTranslatedText('tacosName3');
+      this.tacosInfo3 = this.getTranslatedText('tacosInfo3');
+
+      this.enchiladasName0 = this.getTranslatedText('enchiladasName0');
+      this.enchiladasInfo0 = this.getTranslatedText('enchiladasInfo0');
+      this.enchiladasName1 = this.getTranslatedText('enchiladasName0');
+      this.enchiladasInfo1 = this.getTranslatedText('enchiladasInfo0');
+      this.enchiladasName2 = this.getTranslatedText('enchiladasName0');
+      this.enchiladasInfo2 = this.getTranslatedText('enchiladasInfo0');
+
+      this.dessertsName0 = this.getTranslatedText('dessertsName0');
+      this.dessertsInfo0 = this.getTranslatedText('dessertsInfo0');
+      this.dessertsName1 = this.getTranslatedText('dessertsName0');
+      this.dessertsInfo1 = this.getTranslatedText('dessertsInfo0');
+      this.dessertsName2 = this.getTranslatedText('dessertsName0');
+      this.dessertsInfo2 = this.getTranslatedText('dessertsInfo0');
+
+      this.beveragesName0 = this.getTranslatedText('beveragesName0');
+      this.beveragesInfo0 = this.getTranslatedText('beveragesInfo0');
+      this.beveragesName1 = this.getTranslatedText('beveragesName1');
+      this.beveragesInfo1 = this.getTranslatedText('beveragesInfo1');
+      this.beveragesName2 = this.getTranslatedText('beveragesName2');
+      this.beveragesInfo2 = this.getTranslatedText('beveragesInfo2');
     });
   }
 
@@ -246,7 +336,95 @@ export class MenuComponent{
         es: translation.Language.es.menu.soupsAndSalads[2].description
       },
 
-      
+      //Tacos
+      tacosName0: {
+        en: translation.Language.en.menu.tacos[0].Name,
+        es: translation.Language.es.menu.tacos[0].Name
+      },tacosInfo0: {
+        en: translation.Language.en.menu.tacos[0].description,
+        es: translation.Language.es.menu.tacos[0].description
+      },tacosName1: {
+        en: translation.Language.en.menu.tacos[1].Name,
+        es: translation.Language.es.menu.tacos[1].Name
+      },tacosInfo1: {
+        en: translation.Language.en.menu.tacos[1].description,
+        es: translation.Language.es.menu.tacos[1].description
+      },tacosName2: {
+        en: translation.Language.en.menu.tacos[2].Name,
+        es: translation.Language.es.menu.tacos[2].Name
+      },tacosInfo2: {
+        en: translation.Language.en.menu.tacos[2].description,
+        es: translation.Language.es.menu.tacos[2].description
+      },tacosName3: {
+        en: translation.Language.en.menu.tacos[3].Name,
+        es: translation.Language.es.menu.tacos[3].Name
+      },tacosInfo3: {
+        en: translation.Language.en.menu.tacos[3].description,
+        es: translation.Language.es.menu.tacos[3].description
+      },
+
+      //Enchiladas
+      enchiladasName0: {
+        en: translation.Language.en.menu.enchiladas[0].Name,
+        es: translation.Language.es.menu.enchiladas[0].Name,
+      },enchiladasInfo0: {
+        en: translation.Language.en.menu.enchiladas[0].description,
+        es: translation.Language.es.menu.enchiladas[0].description,
+      },enchiladasName1: {
+        en: translation.Language.en.menu.enchiladas[1].Name,
+        es: translation.Language.es.menu.enchiladas[1].Name,
+      },enchiladasInfo1: {
+        en: translation.Language.en.menu.enchiladas[1].description,
+        es: translation.Language.es.menu.enchiladas[1].description,
+      },enchiladasName2: {
+        en: translation.Language.en.menu.enchiladas[2].Name,
+        es: translation.Language.es.menu.enchiladas[2].Name,
+      },enchiladasInfo2: {
+        en: translation.Language.en.menu.enchiladas[2].description,
+        es: translation.Language.es.menu.enchiladas[2].description,
+      },
+
+      //Postres
+      dessertsName0: {
+        en: translation.Language.en.menu.desserts[0].Name,
+        es: translation.Language.es.menu.desserts[0].Name,
+      },dessertsInfo0: {
+        en: translation.Language.en.menu.desserts[0].description,
+        es: translation.Language.es.menu.desserts[0].description,
+      },dessertsName1: {
+        en: translation.Language.en.menu.desserts[1].Name,
+        es: translation.Language.es.menu.desserts[1].Name,
+      },dessertsInfo1: {
+        en: translation.Language.en.menu.desserts[1].description,
+        es: translation.Language.es.menu.desserts[1].description,
+      },dessertsName2: {
+        en: translation.Language.en.menu.desserts[2].Name,
+        es: translation.Language.es.menu.desserts[2].Name,
+      },dessertsInfo2: {
+        en: translation.Language.en.menu.desserts[2].description,
+        es: translation.Language.es.menu.desserts[2].description,
+      },
+
+      //Beverages
+      beveragesName0: {
+        en: translation.Language.en.menu.beverages[0].Name,
+        es: translation.Language.es.menu.beverages[0].Name,
+      },beveragesInfo0: {
+        en: translation.Language.en.menu.beverages[0].description,
+        es: translation.Language.es.menu.beverages[0].description,
+      },beveragesName1: {
+        en: translation.Language.en.menu.beverages[1].Name,
+        es: translation.Language.es.menu.beverages[1].Name,
+      },beveragesInfo1: {
+        en: translation.Language.en.menu.beverages[1].description,
+        es: translation.Language.es.menu.beverages[1].description,
+      },beveragesName2: {
+        en: translation.Language.en.menu.beverages[2].Name,
+        es: translation.Language.es.menu.beverages[2].Name,
+      },beveragesInfo2: {
+        en: translation.Language.en.menu.beverages[2].description,
+        es: translation.Language.es.menu.beverages[2].description,
+      },
     };
 
     const currentLanguage = this.languageService.getCurrentLanguage();
